ea99cd71ba34ddaf7fb4baee8f939bbd2b0a8c6f
Abhay <abhay@indusnet.co.in>
2022-07-15 18:54:45 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with abhay
+          Learn React with abhay Singh
         </a>
       </header>
     </div>
